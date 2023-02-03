69723b46601eb56f2cc67d4b494ddb79162f32e7
DariaLeonova <darja.leonova.it@gmail.com>
2023-02-03 13:22:52 +0200

    #6: Changed Execute() func.

--- cmd/translate/root.go
+++ cmd/translate/root.go
@@ -23,8 +23,7 @@ var rootCmd = &cobra.Command{
 // Execute adds all child commands to the root command and sets flags appropriately.
 // This is called by main.main(). It only needs to happen once to the rootCmd.
 func Execute() {
-	err := rootCmd.Execute()
-	if err != nil {
+	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
 	}
 }
@@ -50,5 +49,7 @@ func initConfig() {
 	// If a config file is found, read it in.
 	if err := viper.ReadInConfig(); err == nil {
 		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	} else {
+		fmt.Printf("%v", err)
 	}
 }
