4deaecf22f9b54429f5d527580fa27915b666f8d
DarjaLeonova <94634373+DarjaLeonova@users.noreply.github.com>
2023-02-06 10:23:15 +0200

    #6: Added translate command, earthly, golangci and added Github workflow (#7)
    * #6: Added translate command, earthly, golangci and added Github workflow

--- cmd/translate/main.go
+++ cmd/translate/main.go
@@ -1,3 +1,61 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var cfgFile string
+
+// rootCmd represents the base command when called without any subcommands.
+var rootCmd = &cobra.Command{
+	Use:   "translate",
+	Short: "Enables translation for Cloud-native systems",
+	Long:  `Enables translation for Cloud-native systems`,
+	// Uncomment the following line if your bare application
+	// has an action associated with it:
+	// Run: func(cmd *cobra.Command, args []string) { },
+}
+
+func main() {
+	Execute()
+}
+
+// Execute adds all child commands to the root command and sets flags appropriately.
+// This is called by main.main(). It only needs to happen once to the rootCmd.
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		os.Exit(1)
+	}
+}
+
+func init() {
+	cobra.OnInitialize(initConfig)
+	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is examples/translate.yml)")
+}
+
+// initConfig reads in config file and ENV variables if set.
+func initConfig() {
+	if cfgFile != "" {
+		// Use config file from the flag.
+		viper.SetConfigFile(cfgFile)
+	} else {
+		// Find current dir.
+		dir, err := os.Getwd()
+		cobra.CheckErr(err)
+
+		// Search config in current directory with name ".translate.yml".
+		viper.AddConfigPath(dir)
+		viper.SetConfigFile("translate.yml")
+	}
+
+	viper.AutomaticEnv() // read in environment variables that match
+
+	// If a config file is found, read it in.
+	if err := viper.ReadInConfig(); err == nil {
+		fmt.Fprintln(os.Stderr, "Using config file:", viper.ConfigFileUsed())
+	}
+}
