4d69e5c5479cacfea674aa70288c9468f9a9fe93
Vladislavs <vladislavs.perkanuks@responsivecraft.com>
2023-02-13 18:23:39 +0200

    #17 Updated tests

--- pkg/convert/arb_test.go
+++ pkg/convert/arb_test.go
@@ -21,25 +21,26 @@ func Test_FromArb(t *testing.T) {
 		wantErr bool
 	}{
 		{
-			name: "a",
+			name: "Combination of messages",
 			args: args{
 				in0: []byte(`
 				{
 					"title": "Hello World!",
-					"@title" : {
-						"description" : "Message to greet the World"
+					"@title": {
+						"description": "Message to greet the World"
 					},
 					"greeting": "Welcome {user}!",
 					"@greeting": {
 						"placeholders": {
-							"user":{
-								"type":"string",
-								"example":"Bob"
+							"user": {
+								"type": "string",
+								"example": "Bob"
 							}
 						}
 					},
-					"aaa":"sss" 
-				}`),
+					"farewell": "Goodbye friend"
+				}
+				`),
 			},
 			want: model.Messages{
 				Messages: []model.Message{
@@ -53,12 +54,54 @@ func Test_FromArb(t *testing.T) {
 						Message: "Welcome {user}!",
 					},
 					{
-						ID:      "aaa",
-						Message: "sss",
+						ID:      "farewell",
+						Message: "Goodbye friend",
 					},
 				},
 			},
 			wantErr: false,
+		},
+		{
+			name: "Wrong value type for @key",
+			args: args{
+				in0: []byte(`
+				{
+					"title": "Hello World!",
+					"@title": "Message to greet the World"
+				}
+					`),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Wrong value type for standard key",
+			args: args{
+				in0: []byte(`
+				{
+					"title": "Hello World!",
+					"greeting": {
+						"description": "Needed for greeting"
+					}
+				}
+					`),
+			},
+			wantErr: true,
+		},
+		{
+			name: "Wrong value type for description key",
+			args: args{
+				in0: []byte(`
+				{
+					"title": "Hello World!",
+					"@title": {
+						"description": {
+							"meaning": "When you greet someone"
+						}
+					}
+				}
+					`),
+			},
+			wantErr: true,
 		},
 	}
 	for _, tt := range tests {
@@ -67,10 +110,18 @@ func Test_FromArb(t *testing.T) {
 			t.Parallel()
 
 			res, err := FromArb(tt.args.in0)
+
+			if tt.wantErr {
+				assert.Error(t, err)
+				return
+			}
+
 			if !assert.NoError(t, err) {
 				return
 			}
-			assert.Equal(t, tt.want, res)
+
+			assert.Equal(t, tt.want.Language, res.Language)
+			assert.ElementsMatch(t, tt.want.Messages, res.Messages)
 		})
 	}
 }
@@ -108,84 +159,4 @@ func Test_ToArb(t *testing.T) {
 	}
 
 	assert.JSONEq(t, string(want), string(res))
-
-	// type args struct {
-	// 	messages model.Messages
-	// }
-
-	// tests := []struct {
-	// 	name    string
-	// 	args    args
-	// 	want    []byte
-	// 	wantErr bool
-	// }{
-	// 	{
-	// 		name: "Messages to ARB",
-	// 		args: args{
-	// 			messages: model.Messages{
-	// 				Messages: []model.Message{
-	// 					{
-	// 						ID:          "title",
-	// 						Message:     "Hello World!",
-	// 						Description: "Message to greet the World",
-	// 					},
-	// 					{
-	// 						ID:      "greeting",
-	// 						Message: "Welcome {user}",
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 		want: []byte(`
-	// 		{
-	// 			"title":"Hello World!",
-	// 			"@title":{
-	// 				"description":"Message to greet the World"
-	// 			},
-	// 			"greeting":"Welcome {user}"
-	// 		}
-	// 		`),
-	// 	},
-	// 	{
-	// 		name: "With Description",
-	// 		args: args{
-	// 			messages: model.Messages{
-	// 				Messages: []model.Message{
-	// 					{
-	// 						ID:          "title",
-	// 						Message:     "Hello World!",
-	// 						Description: "Message to greet the World",
-	// 					},
-	// 					{
-	// 						ID:          "greeting",
-	// 						Message:     "Welcome {user}",
-	// 						Description: "Welcome user to web page",
-	// 					},
-	// 				},
-	// 			},
-	// 		},
-	// 		want: []byte(`
-	// 		{
-	// 			"title":"Hello World!",
-	// 			"@title":{
-	// 				"description":"Message to greet the World"
-	// 			},
-	// 			"greeting":"Welcome {user}"
-	// 		}
-	// 		`),
-	// 	},
-	// }
-	// for _, tt := range tests {
-	// 	tt := tt
-	// 	t.Run(tt.name, func(t *testing.T) {
-	// 		t.Parallel()
-
-	// 		res, err := ToArb(tt.args.messages)
-	// 		if !assert.NoError(t, err) {
-	// 			return
-	// 		}
-
-	// 		assert.JSONEq(t, string(tt.want), string(res))
-	// 	})
-	// }
 }
