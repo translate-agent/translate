dcfc8f7c9aecffe9e0f68c6d926bd674d61a16f2
DariaLeonova <darja.leonova.it@gmail.com>
2023-02-13 11:00:53 +0200

    #15 Transform to and from ngx translate

---
+++ pkg/convert/json_ngxtranslate.go
@@ -0,0 +1,61 @@
+package convert
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+
+	"go.expect.digital/translate/pkg/model"
+)
+
+func FromNgxTranslate(b []byte) (messages model.Messages, err error) {
+	var dst map[string]interface{}
+
+	if err = json.Unmarshal(b, &dst); err != nil {
+		return messages, fmt.Errorf("unmarshal ngx translateOld: %w", err)
+	}
+
+	var traverseMap func(key string, value interface{}) error
+
+	traverseMap = func(key string, value interface{}) (err error) {
+		switch v := value.(type) {
+		default:
+			return errors.New("wrong key type")
+		case string:
+			messages.Messages = append(messages.Messages, model.Message{ID: key, Message: v})
+		case map[string]interface{}:
+			for subKey, subValue := range v {
+				if key != "" {
+					subKey = key + "." + subKey
+				}
+
+				if err = traverseMap(subKey, subValue); err != nil {
+					return err
+				}
+			}
+		}
+
+		return err
+	}
+
+	if err = traverseMap("", dst); err != nil {
+		return messages, fmt.Errorf("traverse ngx: %w", err)
+	}
+
+	return messages, nil
+}
+
+func ToNgxTranslate(messages model.Messages) (b []byte, err error) {
+	dst := make(map[string]string, len(messages.Messages))
+
+	for _, msg := range messages.Messages {
+		dst[msg.ID] = msg.Message
+	}
+
+	b, err = json.Marshal(dst)
+	if err != nil {
+		return nil, fmt.Errorf("marshal to ngx translateOld : %w", err)
+	}
+
+	return b, nil
+}
---
+++ pkg/convert/json_ngxtranslate_test.go
@@ -0,0 +1,129 @@
+package convert
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"go.expect.digital/translate/pkg/model"
+)
+
+func Test_fromNgxTranslate(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name       string
+		serialized []byte
+		expected   model.Messages
+		wantErr    bool
+	}{
+		{
+			name:       "Not nested",
+			serialized: []byte(`{"message":"example"}`),
+			expected: model.Messages{
+				Messages: []model.Message{
+					{
+						ID:      "message",
+						Message: "example",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:       "Nested normally",
+			serialized: []byte(`{"message":{"example":"message1"}}`),
+			expected: model.Messages{
+				Messages: []model.Message{
+					{
+						ID:      "message.example",
+						Message: "message1",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:       "Nested with dot",
+			serialized: []byte(`{"message.example":"message1"}`),
+			expected: model.Messages{
+				Messages: []model.Message{
+					{
+						ID:      "message.example",
+						Message: "message1",
+					},
+				},
+			},
+			wantErr: false,
+		},
+		{
+			name:       "Nested mixed",
+			serialized: []byte(`{"message.example":"message1","message":{"example":"message1"}}`),
+			expected: model.Messages{
+				Messages: []model.Message{
+					{
+						ID:      "message.example",
+						Message: "message1",
+					},
+					{
+						ID:      "message.example",
+						Message: "message1",
+					},
+				},
+			},
+			wantErr: false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result, err := FromNgxTranslate(tt.serialized)
+
+			assert.NoError(t, err)
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
+
+func Test_toNgxTranslate(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		messages model.Messages
+		expected []byte
+		wantErr  bool
+	}{
+		{
+			name: "Messages to NGX-Translate",
+			messages: model.Messages{
+				Messages: []model.Message{
+					{
+						ID:      "message",
+						Message: "example",
+					},
+					{
+						ID:      "message.example",
+						Message: "message1",
+					},
+				},
+			},
+			expected: []byte(`{"message":"example","message.example":"message1"}`),
+			wantErr:  false,
+		},
+	}
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			result, err := ToNgxTranslate(tt.messages)
+			assert.NoError(t, err)
+
+			assert.Equal(t, tt.expected, result)
+		})
+	}
+}
