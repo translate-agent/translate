410c014643fc857bd1b66abc3e010e471842fcf4
Janis Klavins <janis.klavins@expect.digital>
2023-03-29 16:47:23 +0300

    tidy

--- cmd/client/cmd/service.go
+++ cmd/client/cmd/service.go
@@ -14,13 +14,21 @@ import (
 	"google.golang.org/grpc/credentials/insecure"
 )
 
+const establishConnTimeout = 5 * time.Second
+
 func init() {
 	flags := lsCmd.Flags()
 
 	flags.StringP("address", "a", "localhost:8080", `"translate" service address as "host:port"`)
+	flags.BoolP("insecure", "i", false, `disable transport security (default false)`)
+	flags.DurationP("timeout", "t", establishConnTimeout, `timeout for establishing connection with "translate" service`)
 
 	if err := viper.BindPFlag("address", flags.Lookup("address")); err != nil {
 		log.Panicf("bind address flag: %v", err)
+	}
+
+	if err := viper.BindPFlag("timeout", flags.Lookup("timeout")); err != nil {
+		log.Panicf("bind timeout flag: %v", err)
 	}
 
 	serviceCmd.AddCommand(lsCmd)
@@ -42,14 +50,9 @@ var lsCmd = &cobra.Command{
 	Use:   "ls",
 	Short: "Lists services.",
 	Run: func(cmd *cobra.Command, args []string) {
-		addr, err := cmd.Flags().GetString("address")
-		if err != nil {
-			log.Panicf("list services: retrieve service address cli parameter: %v", err)
-		}
-
 		ctx := context.Background()
 
-		client, err := newClientConn(ctx, addr)
+		client, err := newClientConn(ctx, cmd)
 		if err != nil {
 			log.Panicf("list services: new GRPC client connection: %v", err)
 		}
@@ -78,15 +81,33 @@ var lsCmd = &cobra.Command{
 
 // helpers
 
-func newClientConn(ctx context.Context, address string) (*grpc.ClientConn, error) {
-	ctx, cancelFunc := context.WithTimeout(ctx, time.Second*10) //nolint:gomnd
+func newClientConn(ctx context.Context, cmd *cobra.Command) (*grpc.ClientConn, error) {
+	addr, err := cmd.Flags().GetString("address")
+	if err != nil {
+		log.Panicf("list services: retrieve cli parameter 'address': %v", err)
+	}
+
+	timeout, err := cmd.Flags().GetDuration("timeout")
+	if err != nil {
+		log.Panicf("list services: retrieve cli parameter 'timeout': %v", err)
+	}
+
+	isInsecure, err := cmd.Flags().GetBool("insecure")
+	if err != nil {
+		log.Panicf("list services: retrieve cli parameter 'insecure': %v", err)
+	}
+
+	ctx, cancelFunc := context.WithTimeout(ctx, timeout)
 	defer cancelFunc()
 
 	opts := []grpc.DialOption{
-		grpc.WithTransportCredentials(insecure.NewCredentials()),
 		grpc.WithBlock(),
 	}
 
+	if isInsecure {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
 	// Wait for the server to start and establish a connection.
-	return grpc.DialContext(ctx, address, opts...) //nolint:wrapcheck
+	return grpc.DialContext(ctx, addr, opts...) //nolint:wrapcheck
 }
