5c47f05ee660f7bc3276b727161cddc5632f4bd8
Janis Klavins <janis.klavins@expect.digital>
2023-04-17 14:34:01 +0300

    tidy

---
+++ cmd/client/cmd/download.go
@@ -0,0 +1,127 @@
+package cmd
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	translatev1 "go.expect.digital/translate/pkg/pb/translate/v1"
+)
+
+// file formats.
+const (
+	arb  = ".arb"
+	json = ".json"
+	pot  = ".po"
+	xlf  = ".xlf"
+)
+
+func newDownloadCmd() *cobra.Command {
+	var schemaFlag schema
+
+	downloadCmd := &cobra.Command{
+		Use:   "download",
+		Short: "Download file from translate agent service",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			timeout, err := cmd.InheritedFlags().GetDuration("timeout")
+			if err != nil {
+				return fmt.Errorf("download file: get cli parameter 'timeout': %w", err)
+			}
+
+			ctx, cancelFunc := context.WithTimeout(context.Background(), timeout)
+			defer cancelFunc()
+
+			client, err := newClientConn(ctx, cmd)
+			if err != nil {
+				return fmt.Errorf("download file: new GRPC client connection: %w", err)
+			}
+
+			serviceID, err := cmd.Flags().GetString("serviceUUID")
+			if err != nil {
+				return fmt.Errorf("upload file: get cli parameter 'serviceUUID': %w", err)
+			}
+
+			path, err := cmd.Flags().GetString("path")
+			if err != nil {
+				return fmt.Errorf("upload file: get cli parameter 'path': %w", err)
+			}
+
+			language, err := cmd.Flags().GetString("language")
+			if err != nil {
+				return fmt.Errorf("download file: get cli parameter 'language': %w", err)
+			}
+
+			translateSchema, err := schemaFlag.ToTranslateSchema()
+			if err != nil {
+				return fmt.Errorf("download file: schema to translate schema: %w", err)
+			}
+
+			res, err := translatev1.NewTranslateServiceClient(client).DownloadTranslationFile(ctx,
+				&translatev1.DownloadTranslationFileRequest{
+					Language: language, Schema: translateSchema, ServiceId: serviceID,
+				})
+			if err != nil {
+				return fmt.Errorf("download file: send GRPC request: %w", err)
+			}
+
+			fileName := serviceID
+
+			switch translateSchema {
+			case translatev1.Schema_UNSPECIFIED:
+				return errors.New("download file: unspecified file schema")
+			case translatev1.Schema_ARB:
+				fileName += arb
+			case translatev1.Schema_JSON_NG_LOCALIZE, translatev1.Schema_JSON_NGX_TRANSLATE, translatev1.Schema_GO:
+				fileName += json
+			case translatev1.Schema_POT:
+				fileName += pot
+			case translatev1.Schema_XLIFF_12, translatev1.Schema_XLIFF_2:
+				fileName += xlf
+			}
+
+			if err = os.WriteFile(filepath.Join(path, fileName), res.Data, 0644); err != nil { //nolint:gomnd,gofumpt,gosec
+				return fmt.Errorf("download file: write file to path: %w", err)
+			}
+
+			if _, err = fmt.Fprintln(cmd.OutOrStdout(), "File downloaded successfully."); err != nil {
+				return fmt.Errorf("download file: output response to stdout: %w", err)
+			}
+
+			return nil
+		},
+	}
+
+	downloadFlags := downloadCmd.Flags()
+	downloadFlags.StringP("serviceUUID", "u", "", "service UUID")
+	downloadFlags.StringP("path", "p", "", "download folder path")
+	downloadFlags.StringP("language", "l", "", "translation language")
+	downloadFlags.VarP(&schemaFlag, "schema", "s",
+		`translate schema, allowed: 'json_ng_localize', 'json_ngx_translate', 'go', 'arb', 'pot', 'xliff_12', 'xliff_2'`)
+
+	if err := downloadCmd.MarkFlagRequired("serviceUUID"); err != nil {
+		log.Panicf("download file cmd: set field 'serviceUUID' as required: %v", err)
+	}
+
+	if err := downloadCmd.MarkFlagRequired("path"); err != nil {
+		log.Panicf("download file cmd: set field 'path' as required: %v", err)
+	}
+
+	if err := downloadCmd.MarkFlagRequired("language"); err != nil {
+		log.Panicf("download file cmd: set field 'language' as required: %v", err)
+	}
+
+	if err := downloadCmd.MarkFlagRequired("schema"); err != nil {
+		log.Panicf("download file cmd: set field 'schema' as required: %v", err)
+	}
+
+	if err := viper.BindPFlags(downloadFlags); err != nil {
+		log.Panicf("download file cmd: bind flags: %v", err)
+	}
+
+	return downloadCmd
+}
--- cmd/client/cmd/root.go
+++ cmd/client/cmd/root.go
@@ -2,9 +2,15 @@ package cmd
 
 import (
 	"bytes"
+	"context"
+	"errors"
 	"fmt"
+	"strings"
 
 	"github.com/spf13/cobra"
+	translatev1 "go.expect.digital/translate/pkg/pb/translate/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 func newRootCmd() *cobra.Command {
@@ -50,4 +56,61 @@ func ExecuteWithParams(params []string) ([]byte, error) {
 	}
 
 	return buf.Bytes(), nil
+}
+
+// helpers
+
+func newClientConn(ctx context.Context, cmd *cobra.Command) (*grpc.ClientConn, error) {
+	opts := []grpc.DialOption{
+		grpc.WithBlock(),
+	}
+
+	address, err := cmd.InheritedFlags().GetString("address")
+	if err != nil {
+		return nil, fmt.Errorf("get cli parameter 'address': %w", err)
+	}
+
+	connIsInsecure, err := cmd.InheritedFlags().GetBool("insecure")
+	if err != nil {
+		return nil, fmt.Errorf("get cli parameter 'insecure': %w", err)
+	}
+
+	if connIsInsecure {
+		opts = append(opts, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	}
+
+	// Wait for the server to start and establish a connection.
+	return grpc.DialContext(ctx, address, opts...) //nolint:wrapcheck
+}
+
+type schema string
+
+// String is used both by fmt.Print and by Cobra in help text.
+func (s *schema) String() string {
+	return string(*s)
+}
+
+// Set must have pointer receiver so it doesn't change the value of a copy.
+func (s *schema) Set(v string) error {
+	switch v {
+	case "json_ng_localize", "json_ngx_translate", "go", "arb", "pot", "xliff_12", "xliff_2":
+		*s = schema(v)
+		return nil
+	default:
+		return errors.New(
+			`must be one of "json_ng_localize", "json_ngx_translate", "go", "arb", "pot", "xliff_12", "xliff_2"`)
+	}
+}
+
+// Type is only used in help text.
+func (s *schema) Type() string {
+	return "schema"
+}
+
+func (s schema) ToTranslateSchema() (translatev1.Schema, error) {
+	if schemaNum, ok := translatev1.Schema_value[strings.ToUpper(s.String())]; ok {
+		return translatev1.Schema(schemaNum), nil
+	}
+
+	return translatev1.Schema_UNSPECIFIED, errors.New("schema doesn't match translate schema patterns")
 }
