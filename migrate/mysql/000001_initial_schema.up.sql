6c7021513552314fd8d13025bd945e1a111715a2
Vladislavs <v.perkanuks@gmail.com>
2023-04-21 11:49:40 +0300

    Updated db schema. Updated repo upload/download implementation

--- migrate/mysql/000001_initial_schema.up.sql
+++ migrate/mysql/000001_initial_schema.up.sql
@@ -3,19 +3,22 @@ CREATE TABLE service (
   name VARCHAR(255)
 );
 
-CREATE TABLE messages (
-  id BINARY(16) PRIMARY KEY,
-  service_id BINARY(16) NOT NULL,
-  language VARCHAR(20),
-  FOREIGN KEY (service_id) REFERENCES service (id)
-);
-
 CREATE TABLE message (
   service_id BINARY(16) NOT NULL,
-  id VARCHAR(255),
+  language VARCHAR(20),
+  FOREIGN KEY (service_id) REFERENCES service (id),
+  PRIMARY KEY (service_id, language)
+);
+
+CREATE TABLE message_message (
+  message_service_id BINARY(16) NOT NULL,
+  message_language VARCHAR(20) NOT NULL,
+  id TEXT,
   message TEXT NOT NULL,
   description TEXT NOT NULL,
   fuzzy TINYINT(1) NOT NULL,
-  PRIMARY KEY (service_id, id),
-  FOREIGN KEY (service_id) REFERENCES messages (service_id)
+  FOREIGN KEY (message_service_id, message_language) REFERENCES message (
+    service_id, language
+  ),
+  PRIMARY KEY (message_language, id (100))
 );
--- pkg/repo/mysql/translation_file.go
+++ pkg/repo/mysql/translation_file.go
@@ -2,165 +2,97 @@ package mysql
 
 import (
 	"context"
-	"database/sql"
-	"database/sql/driver"
-	"encoding/json"
-	"errors"
 	"fmt"
 
 	"github.com/google/uuid"
 	"go.expect.digital/translate/pkg/model"
-	"go.expect.digital/translate/pkg/repo"
 	"golang.org/x/text/language"
 )
 
-type dbMessageSlice []model.Message
-
-type dbLanguageTag struct {
-	language.Tag
-}
-
-type dbTranslationFile struct {
-	lang     dbLanguageTag
-	messages dbMessageSlice
-	id       uuid.UUID
-}
-
-// fromTranslationFile converts model.TranslationFile to dbTranslationFile.
-func fromTranslationFile(translationFile *model.TranslationFile) *dbTranslationFile {
-	return &dbTranslationFile{
-		lang:     dbLanguageTag{Tag: translationFile.Messages.Language},
-		messages: translationFile.Messages.Messages,
-		id:       translationFile.ID,
-	}
-}
-
-// toTranslationFile converts dbTranslationFile to model.TranslationFile.
-func toTranslationFile(translationFile *dbTranslationFile) *model.TranslationFile {
-	return &model.TranslationFile{
-		ID: translationFile.id,
-		Messages: model.Messages{
-			Language: translationFile.lang.Tag,
-			Messages: translationFile.messages,
-		},
-	}
-}
-
-// ---------------Messages Scanner/Valuer--------------.
-
-func (d *dbMessageSlice) Scan(src any) error {
-	data, ok := src.([]byte)
-	if !ok {
-		return fmt.Errorf("unsupported type '%T'", src)
-	}
-
-	var messages dbMessageSlice
-
-	if err := json.Unmarshal(data, &messages); err != nil {
-		return fmt.Errorf("unmarshal messages: %w", err)
-	}
-
-	*d = messages
-
-	return nil
-}
-
-func (d dbMessageSlice) Value() (driver.Value, error) {
-	data, err := json.Marshal(d)
-	if err != nil {
-		return nil, fmt.Errorf("marshal messages: %w", err)
-	}
-
-	return data, nil
-}
-
-// ---------------Language Scanner/Valuer--------------
-
-func (d *dbLanguageTag) Scan(src any) error {
-	data, ok := src.([]uint8)
-	if !ok {
-		return fmt.Errorf("unsupported type '%T'", src)
-	}
-
-	str := string(data)
-
-	var err error
-
-	d.Tag, err = language.Parse(str)
-	if err != nil {
-		return fmt.Errorf("parse language: %w", err)
-	}
-
-	return nil
-}
-
-func (d dbLanguageTag) Value() (driver.Value, error) {
-	return d.Tag.String(), nil
-}
-
 //--------------------Repo Implementation--------------------
 
-func (r *Repo) SaveTranslationFile(
-	ctx context.Context,
-	serviceID uuid.UUID,
-	translationFile *model.TranslationFile,
-) error {
+func (r *Repo) SaveMessages(ctx context.Context, serviceID uuid.UUID, messages *model.Messages) error {
 	_, err := r.LoadService(ctx, serviceID)
 	if err != nil {
 		return fmt.Errorf("repo: load service: %w", err)
 	}
 
-	if translationFile.ID == uuid.Nil {
-		translationFile.ID = uuid.New()
+	tx, err := r.db.Begin()
+	if err != nil {
+		return fmt.Errorf("repo: begin tx: %w", err)
 	}
 
-	dbFile := fromTranslationFile(translationFile)
+	defer tx.Rollback() //nolint:errcheck
 
-	query := `INSERT INTO translation_file (
-		id, service_id, 
-		language, messages
-		) 
-		VALUES (
-		UUID_TO_BIN(?), UUID_TO_BIN(?), 
-		?, ?
-		)
-		ON DUPLICATE KEY UPDATE
-    messages = VALUES(messages);`
-
-	_, err = r.db.ExecContext(ctx, query,
-		dbFile.id, serviceID,
-		dbFile.lang, dbFile.messages,
+	// Insert into messages table
+	_, err = tx.Exec(
+		`INSERT IGNORE INTO message (service_id, language) VALUES (UUID_TO_BIN(?), ?)`,
+		serviceID.String(),
+		messages.Language.String(),
 	)
-
 	if err != nil {
-		return fmt.Errorf("repo: insert translate file: %w", err)
+		return fmt.Errorf("repo: insert messages: %w", err)
+	}
+
+	// Insert into message_message table
+	stmt, err := tx.Prepare(
+		`INSERT INTO message_message 
+		(message_service_id, message_language, id, message, description, fuzzy) 
+	VALUES 
+		(UUID_TO_BIN(?), ?, ?, ?, ?, ?) 
+	ON DUPLICATE KEY UPDATE 
+		message = VALUES(message), 
+		description = VALUES(description), 
+		fuzzy = VALUES(fuzzy)`,
+	)
+	if err != nil {
+		return fmt.Errorf("repo: prepare insert statement: %w", err)
+	}
+	defer stmt.Close()
+
+	for _, m := range messages.Messages {
+		_, err = stmt.Exec(serviceID.String(), messages.Language.String(), m.ID, m.Message, m.Description, m.Fuzzy)
+		if err != nil {
+			return fmt.Errorf("repo: insert message_message: %w", err)
+		}
+	}
+
+	if err := tx.Commit(); err != nil {
+		return fmt.Errorf("repo: commit tx: %w", err)
 	}
 
 	return nil
 }
 
-func (r *Repo) LoadTranslationFile(
-	ctx context.Context,
-	serviceID uuid.UUID,
-	language language.Tag) (
-	*model.TranslationFile, error,
-) {
-	query := `SELECT id, language, messages FROM translation_file WHERE service_id = UUID_TO_BIN(?) AND language = ?`
-
-	row := r.db.QueryRowContext(ctx, query, serviceID, dbLanguageTag{Tag: language})
-
-	var dbFile dbTranslationFile
-
-	switch err := row.Scan(
-		&dbFile.id,
-		&dbFile.lang,
-		&dbFile.messages,
-	); {
-	default:
-		return toTranslationFile(&dbFile), nil
-	case errors.Is(err, sql.ErrNoRows):
-		return nil, repo.ErrNotFound
-	case err != nil:
-		return nil, fmt.Errorf("repo: select translate file: %w", err)
+func (r *Repo) LoadMessages(ctx context.Context, serviceID uuid.UUID, language language.Tag) (*model.Messages, error) {
+	rows, err := r.db.QueryContext(
+		ctx,
+		`SELECT id, message, description, fuzzy
+			FROM message_message
+			WHERE message_service_id = UUID_TO_BIN(?) AND message_language = ?`,
+		serviceID.String(),
+		language.String(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("repo: query messages: %w", err)
 	}
+
+	defer rows.Close()
+
+	var messages []model.Message
+
+	for rows.Next() {
+		var m model.Message
+		if err := rows.Scan(&m.ID, &m.Message, &m.Description, &m.Fuzzy); err != nil {
+			return nil, fmt.Errorf("repo: scan message: %w", err)
+		}
+
+		messages = append(messages, m)
+	}
+
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("repo: scan messages: %w", err)
+	}
+
+	return &model.Messages{Language: language, Messages: messages}, nil
 }
