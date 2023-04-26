dfbddfac1582fbce4815fcb095035d6f911f4d47
Vladislavs <v.perkanuks@gmail.com>
2023-04-26 10:18:43 +0300

    Added nested message fieldMask validation

--- pkg/translate/field_mask.go
+++ pkg/translate/field_mask.go
@@ -2,30 +2,71 @@ package translate
 
 import (
 	"fmt"
+	"strings"
 
 	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/reflect/protoreflect"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
 )
 
+// validateFieldMask validates that all the field paths in the given fieldMask are valid for the given protoMessage.
 func validateFieldMask(fieldMask *fieldmaskpb.FieldMask, protoMessage proto.Message) error {
+	// Get the message descriptor for the protoMessage
 	messageDescriptor := protoMessage.ProtoReflect().Descriptor()
 
-	// Create a map to store the field names
-	fieldNames := make(map[string]bool)
+	fieldNames := getFieldNames(messageDescriptor)
 
-	// Loop through each field and add the name to the map
-	fields := messageDescriptor.Fields()
-	for i := 0; i < fields.Len(); i++ {
-		fieldNames[string(fields.Get(i).Name())] = true
+	// validateFieldPath validates that the given pathsParts are valid for the given fieldNames.
+	var validateFieldPath func([]string, map[string]protoreflect.FieldDescriptor) error
+
+	validateFieldPath = func(pathParts []string, fieldNames map[string]protoreflect.FieldDescriptor) error {
+		// If there are no more path parts to validate, return nil
+		if len(pathParts) == 0 {
+			return nil
+		}
+
+		// Get the first path part and check if it is a valid field name
+		fieldName := pathParts[0]
+
+		fieldDescriptor, ok := fieldNames[fieldName]
+		if !ok {
+			return fmt.Errorf("invalid field '%s'", fieldName)
+		}
+
+		// If there are more path parts and the current field is a nested message,
+		// create a new map of field names for the nested message and call validateFieldPath recursively
+		if length, msg := len(pathParts), fieldDescriptor.Message(); length > 1 && msg != nil {
+			nestedFieldNames := getFieldNames(msg)
+			return validateFieldPath(pathParts[1:], nestedFieldNames)
+		}
+
+		return nil
 	}
 
-	// Loop through each path in the field mask
+	// Loop through each path in the field mask and split it into parts
 	for _, path := range fieldMask.GetPaths() {
-		// Check if the path is a valid field for the proto message
-		if _, ok := fieldNames[path]; !ok {
-			return fmt.Errorf("'%s' is not a valid %s field", path, messageDescriptor.Name())
+		pathParts := strings.Split(path, ".")
+		if err := validateFieldPath(pathParts, fieldNames); err != nil {
+			return fmt.Errorf("'%s' is not a valid %s field: %w", path, messageDescriptor.Name(), err)
 		}
 	}
 
 	return nil
+}
+
+// getFieldNames returns a map of field names for the given message descriptor.
+func getFieldNames(descriptor protoreflect.MessageDescriptor) map[string]protoreflect.FieldDescriptor {
+	fields := descriptor.Fields()
+
+	// Create a map to store the field names
+	n := fields.Len()
+	fieldNames := make(map[string]protoreflect.FieldDescriptor, n)
+
+	// Loop through each field in the message descriptor and add its name to the map
+	for i := 0; i < n; i++ {
+		field := fields.Get(i)
+		fieldNames[string(field.Name())] = field
+	}
+
+	return fieldNames
 }
--- pkg/translate/field_mask_test.go
+++ pkg/translate/field_mask_test.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/brianvoe/gofakeit/v6"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	translatev1 "go.expect.digital/translate/pkg/pb/translate/v1"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/fieldmaskpb"
@@ -29,9 +30,19 @@ func Test_ValidateFieldMask(t *testing.T) {
 		protoMessage: &translatev1.Message{},
 	}
 
+	happyPathNestedCreateReq := args{
+		fieldMask:    &fieldmaskpb.FieldMask{Paths: []string{"service.id"}},
+		protoMessage: &translatev1.CreateServiceRequest{},
+	}
+
+	happyPathNestedUpdateReq := args{
+		fieldMask:    &fieldmaskpb.FieldMask{Paths: []string{"update_mask.paths"}},
+		protoMessage: &translatev1.UpdateServiceRequest{},
+	}
+
 	randomFieldMaskPathMessage := args{
-		fieldMask:    &fieldmaskpb.FieldMask{Paths: []string{gofakeit.FirstName()}},
-		protoMessage: &translatev1.Message{},
+		fieldMask:    &fieldmaskpb.FieldMask{Paths: []string{"service." + gofakeit.FirstName()}},
+		protoMessage: &translatev1.UpdateServiceRequest{},
 	}
 
 	tests := []struct {
@@ -47,6 +58,16 @@ func Test_ValidateFieldMask(t *testing.T) {
 		{
 			name:        "Happy Path Message",
 			args:        happyPathMessage,
+			expectedErr: nil,
+		},
+		{
+			name:        "Happy Path Nested Create Request",
+			args:        happyPathNestedCreateReq,
+			expectedErr: nil,
+		},
+		{
+			name:        "Happy Path Nested Update Request",
+			args:        happyPathNestedUpdateReq,
 			expectedErr: nil,
 		},
 		{
@@ -67,6 +88,47 @@ func Test_ValidateFieldMask(t *testing.T) {
 			}
 
 			assert.NoError(t, err)
+		})
+	}
+}
+
+func Test_GetFieldNames(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		protoMsg proto.Message
+		expected []string
+	}{
+		{
+			name:     "Service",
+			protoMsg: &translatev1.Service{},
+			expected: []string{"id", "name"},
+		},
+		{
+			name:     "Message",
+			protoMsg: &translatev1.Message{},
+			expected: []string{"id", "message", "description", "fuzzy"},
+		},
+		{
+			name:     "Messages",
+			protoMsg: &translatev1.Messages{},
+			expected: []string{"language", "messages"},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			actualFieldNames := getFieldNames(tt.protoMsg.ProtoReflect().Descriptor())
+
+			require.Len(t, actualFieldNames, len(tt.expected))
+
+			for _, expected := range tt.expected {
+				require.Contains(t, actualFieldNames, expected)
+			}
 		})
 	}
 }
