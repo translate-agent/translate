dc3a79db975cb185bd3ee71fb16b29a5ad3a94c0
Vladislavs Perkaņuks <69954124+VladislavsPerkanuks@users.noreply.github.com>
2023-05-03 12:54:13 +0300

    Translation File Upload/Download Implementation (#52)
    * #40 Add initial repo implementation
    
    * #40 Initial implementation of `CRUD` operations for services
    
    * #40 Tidy
    
    * Go mod tidy
    
    * Separated `Address` -> `Host` and `Port`
    
    * Added otel to DB operations
    
    * Switched to another otelsql package
    
    * Added sql linter
    
    * Updated `services.go`
    
    * Updated tests
    
    * #40 tidy
    
    * tidy
    
    * added ctx param to 'insertTestService'
    
    * Initial handler CRUD implementation
    
    * TIDY
    
    * Initial handler Implementation
    
    * TIDY
    
    * Changed `dbms`->`db`
    
    * Initial commit
    
    * TIDY
    
    * TIDY
    
    * Updated repo tests
    
    * TIDY
    
    * Tidy env.test
    
    * Updated gitignore
    
    * Updated Earthfile and tests
    
    * TIDY
    
    * Updated dependencies
    
    * Fixed `PATCH` request
    
    * Updated `Github Actions`
    
    * added allow privileged for integration tests
    
    * Updated tests
    
    * TIDY
    
    * Updated proto
    
    * Updated proto Schemas
    
    * Updated `launch.json`
    
    * Translate file initial repo implementation
    
    * Moved Scanner and Valuer to mysql package
    
    * Changed TranslateFile struct
    
    * Implemented upload/download handler functionality + updated params parsers
    
    * moved convert to `translate` pkg
    
    * Added download/upload file integration tests
    
    * Changed asserts to return more info on Failure
    
    * Updated file_test
    
    * Changed translateFile -> translationFile
    
    * Updated tests with require
    
    * Refactored tests
    
    * Updated dependencies + using images instead of binaries for linting
    
    * Typo fix
    
    * Removed comment
    
    * Added supported db list. Created new package transform
    
    * Moved transform to translate pkg. Added transform types. Added Tests. Updated service handlers with transform functions
    
    * Updated tests with random values. Added missing parseCreateServiceParams test.
    
    * Changed model.Service in params to a pointer
    
    * Refactored main integration tests
    
    * Updated Earthly version in CI/CD
    
    * Upgraded go dependencies
    
    * Github actions env fix
    
    * Omit POT Schema when generating rand data, to fix CI/CD
    
    * Tidy
    
    * Changed NG_LOCALISE -> NG_LOCALIZE
    
    * Updated request param parser. Added and refactored tests
    
    * Replaced unused argument with placeholder
    
    * Ensure that in tests notFoundLanguageRequest, has different language than original
    
    * Updated params parser to return pointer
    
    * Updated req param parser to return pointer
    
    * Added Scanner/Valuer tests
    
    * Updated transform with generic function for slices. Refactored transform tests
    
    * Updated service request param parser. Updated tests
    
    * Bumped sqlfluff version
    
    * Updated Upload/Download request param parser
    
    * TIDY
    
    * Updated go.mod
    
    * Moved migrate sql to the one file
    
    * TIDY
    
    * Updated db schema. Updated repo upload/download implementation
    
    * Updated grpc/rest integration tests
    
    * Updated migrate down sql script
    
    * TIDY
    
    * Tidy
    
    * Added context to LoadMessages
    
    * fmt.Errorf -> errors.New for unformated errors
    
    * Removed golines
    
    * Added comment about POT conversion
    
    * Renamed translation_file -> message
    
    * Declare body.Buffer as zero value variable
    
    * Prettify sql statements for tracing
    
    * renamed prepareService -> createService
    
    * Updated mysql tables. repo Save/Load messages
    
    * Updated DDL. Updated tests.
    
    * Fixed language collision in LoadMessage missingLang test
    
    * Removed redundant message_service_id column
    
    * Changed tx error messages
    
    * Use uuid valuer and scanner interface
    
    * Changed INSERT IGNORE to switch statement
    
    * LoadMessages with JOIN
    
    * Go update in Earthfile
    
    * TIDY
    
    * TIDY
    
    * MD5 -> SHA1

---
+++ pkg/repo/mysql/message_integration_test.go
@@ -0,0 +1,240 @@
+//go:build integration
+
+package mysql
+
+import (
+	"context"
+	"testing"
+
+	"github.com/brianvoe/gofakeit/v6"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"go.expect.digital/translate/pkg/model"
+	"go.expect.digital/translate/pkg/repo"
+	"golang.org/x/text/language"
+)
+
+func randMessages() *model.Messages {
+	size := gofakeit.IntRange(0, 5)
+
+	messages := make([]model.Message, 0, size)
+
+	for i := 0; i < size; i++ {
+		messages = append(messages, model.Message{
+			ID:          gofakeit.SentenceSimple(),
+			Message:     gofakeit.SentenceSimple(),
+			Description: gofakeit.SentenceSimple(),
+			Fuzzy:       gofakeit.Bool(),
+		},
+		)
+	}
+
+	return &model.Messages{Language: language.MustParse(gofakeit.LanguageBCP()), Messages: messages}
+}
+
+func prepareService(ctx context.Context, t *testing.T) *model.Service {
+	t.Helper()
+
+	service := randService()
+
+	err := repository.SaveService(ctx, service)
+	require.NoError(t, err, "Prepare test service")
+
+	return service
+}
+
+func requireEqualMessages(t *testing.T, expected, actual *model.Messages) {
+	t.Helper()
+
+	require.Equal(t, expected.Language, actual.Language)
+	require.ElementsMatch(t, expected.Messages, actual.Messages)
+}
+
+func Test_SaveMessages(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	service := prepareService(ctx, t)
+
+	tests := []struct {
+		name        string
+		serviceID   uuid.UUID
+		messages    *model.Messages
+		expectedErr error
+	}{
+		{
+			name:        "Happy path",
+			serviceID:   service.ID,
+			messages:    randMessages(),
+			expectedErr: nil,
+		},
+		{
+			name:        "Missing service",
+			serviceID:   uuid.New(),
+			messages:    randMessages(),
+			expectedErr: repo.ErrNotFound,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			err := repository.SaveMessages(ctx, tt.serviceID, tt.messages)
+
+			if tt.expectedErr != nil {
+				assert.ErrorIs(t, err, tt.expectedErr)
+				return
+			}
+
+			require.NoError(t, err, "Save messages")
+
+			// Assure that the messages were saved correctly.
+
+			actualMessages, err := repository.LoadMessages(ctx, tt.serviceID, tt.messages.Language)
+			require.NoError(t, err, "Load saved messages")
+
+			requireEqualMessages(t, tt.messages, actualMessages)
+		})
+
+	}
+}
+
+func Test_SaveMessagesMultipleLangOneService(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	service := prepareService(ctx, t)
+
+	count := gofakeit.IntRange(3, 5)
+	messages := make([]*model.Messages, 0, count)
+
+	languagesUsed := make(map[language.Tag]bool, count)
+
+	// Create messages with different languages
+	for i := 0; i < count; i++ {
+		msg := randMessages()
+
+		// Make sure we don't use the same language twice.
+		for languagesUsed[msg.Language] {
+			msg = randMessages()
+		}
+
+		languagesUsed[msg.Language] = true
+		messages = append(messages, msg)
+	}
+
+	// Save messages
+	for _, m := range messages {
+		err := repository.SaveMessages(ctx, service.ID, m)
+		require.NoError(t, err, "Save messages")
+	}
+
+	// Assure that all messages are saved
+	for _, m := range messages {
+		actualMessages, err := repository.LoadMessages(ctx, service.ID, m.Language)
+		require.NoError(t, err, "Load saved messages")
+
+		requireEqualMessages(t, m, actualMessages)
+	}
+}
+
+func Test_SaveMessagesUpdate(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	// Prepare
+
+	service := prepareService(ctx, t)
+
+	expectedMessages := randMessages()
+
+	err := repository.SaveMessages(ctx, service.ID, expectedMessages)
+	require.NoError(t, err, "Save messages")
+
+	// Actual Test
+
+	// Update Message, Description and Fuzzy values, while keeping the ID
+	for i := range expectedMessages.Messages {
+		expectedMessages.Messages[i].Message = gofakeit.SentenceSimple()
+		expectedMessages.Messages[i].Description = gofakeit.SentenceSimple()
+		expectedMessages.Messages[i].Fuzzy = gofakeit.Bool()
+
+	}
+
+	// Save updated messages
+
+	err = repository.SaveMessages(ctx, service.ID, expectedMessages)
+	require.NoError(t, err, "Update messages")
+
+	// Assure that messages are updated
+
+	actualMessages, err := repository.LoadMessages(ctx, service.ID, expectedMessages.Language)
+	require.NoError(t, err, "Load updated messages")
+
+	requireEqualMessages(t, expectedMessages, actualMessages)
+}
+
+func Test_LoadMessages(t *testing.T) {
+	t.Parallel()
+
+	ctx := context.Background()
+
+	service := prepareService(ctx, t)
+
+	messages := randMessages()
+
+	err := repository.SaveMessages(ctx, service.ID, messages)
+	require.NoError(t, err, "Prepare test messages")
+
+	missingServiceID := uuid.New()
+	missingLang := language.MustParse(gofakeit.LanguageBCP())
+	// Make sure we don't use the same language as the message, since that would return not nil Messages.Messages.
+	for missingLang == messages.Language {
+		missingLang = language.MustParse(gofakeit.LanguageBCP())
+	}
+
+	tests := []struct {
+		expected  *model.Messages
+		name      string
+		serviceID uuid.UUID
+		language  language.Tag
+	}{
+		{
+			name:      "Happy Path",
+			expected:  messages,
+			serviceID: service.ID,
+			language:  messages.Language,
+		},
+		{
+			name:      "No messages with service",
+			serviceID: missingServiceID,
+			language:  messages.Language,
+			expected:  &model.Messages{Language: messages.Language, Messages: nil},
+		},
+		{
+			name:      "No messages with language",
+			serviceID: service.ID,
+			language:  missingLang,
+			expected:  &model.Messages{Language: missingLang, Messages: nil},
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			actualMessages, err := repository.LoadMessages(ctx, tt.serviceID, tt.language)
+			require.NoError(t, err, "Load messages")
+
+			requireEqualMessages(t, tt.expected, actualMessages)
+		})
+	}
+}
