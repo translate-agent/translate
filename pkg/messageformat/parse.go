b19a80eda85289ad46cb64406f6fc95e8e0303a4
DarjaLeonova <94634373+DarjaLeonova@users.noreply.github.com>
2023-05-08 11:13:12 +0300

    46# parse message format v2 (#56)
    * Parse MsgFormat2 Lexer
    
    * #46 - Parse MsgFormat2 Lexer
    
    * fixes
    
    * Fixes
    
    * Fixes
    
    * Fixes
    
    * WIP - parse message format
    
    * parse message format
    
    * TIDY
    
    * parse message format
    
    * WIP
    
    * parse message format
    
    * parse message fomat
    
    * TIDY
    
    * parse message format
    
    * parse message format
    
    * parse message format
    
    * parse message format
    
    * new parser
    
    * TIDY
    
    * tidy
    
    * lexer
    
    * parser
    
    * parser for message format v2
    
    * deleted message package
    
    ---------
    
    Co-authored-by: Jānis Horsts <janis.horsts@gmail.com>

---
+++ pkg/messageformat/parse.go
@@ -0,0 +1,245 @@
+package messageformat
+
+import (
+	"errors"
+	"fmt"
+)
+
+type parser struct {
+	lex    *lexer
+	text   string
+	tokens []Token
+	pos    int
+}
+
+func (p *parser) collect() {
+	for {
+		v := p.lex.nextToken()
+
+		switch v.typ {
+		case tokenTypeVariable, tokenTypeFunction, tokenTypeSeparatorOpen,
+			tokenTypeSeparatorClose, tokenTypeText, tokenTypeOpeningFunction,
+			tokenTypeClosingFunction, tokenTypeKeyword, tokenTypeLiteral:
+			p.tokens = append(p.tokens, v)
+		case tokenTypeEOF:
+			p.tokens = append(p.tokens, v)
+			return
+		case tokenTypeError:
+			return
+		}
+	}
+}
+
+func Parse(text string) ([]interface{}, error) {
+	var p parser
+	p.text = text
+	p.lex = lex(text)
+	p.collect()
+
+	tree, err := p.parse()
+	if err != nil {
+		return nil, fmt.Errorf("failed to parse: %w", err)
+	}
+
+	return tree, nil
+}
+
+func (p *parser) parse() ([]interface{}, error) {
+	var tree []interface{}
+
+	for p.pos < len(p.tokens) {
+		token := p.currentToken()
+
+		switch {
+		default:
+			return nil, fmt.Errorf("unknown token: %+v", token)
+		case token.typ == tokenTypeKeyword && token.val == KeywordMatch:
+			match, err := p.parseMatch()
+			if err != nil {
+				return nil, fmt.Errorf("parse match: %w", err)
+			}
+
+			tree = append(tree, match)
+		case token.typ == tokenTypeSeparatorOpen:
+			text, err := p.parseText()
+			if err != nil {
+				return nil, fmt.Errorf("parse text: %w", err)
+			}
+
+			tree = append(tree, text...)
+		case token.typ == tokenTypeEOF:
+			return tree, nil
+		}
+
+		p.pos++
+	}
+
+	return tree, nil
+}
+
+func (p *parser) parseText() ([]interface{}, error) {
+	if p.currentToken().typ != tokenTypeSeparatorOpen {
+		return nil, errors.New(`text does not start with "{"`)
+	}
+
+	var text []interface{}
+
+	for !p.isEOF() {
+		token := p.nextToken()
+
+		switch token.typ {
+		case tokenTypeText:
+			text = append(text, NodeText{Text: token.val})
+		case tokenTypeSeparatorOpen:
+			variable, err := p.parseVariable()
+			if err != nil {
+				return nil, fmt.Errorf("parse variable: %w", err)
+			}
+
+			text = append(text, variable)
+			p.pos++
+		case tokenTypeSeparatorClose:
+			p.pos++
+
+			return text, nil
+		case tokenTypeKeyword, tokenTypeLiteral,
+			tokenTypeFunction, tokenTypeVariable,
+			tokenTypeEOF, tokenTypeError,
+			tokenTypeOpeningFunction, tokenTypeClosingFunction:
+		}
+	}
+
+	return nil, fmt.Errorf("invalid text node")
+}
+
+func (p *parser) parseMatch() (NodeMatch, error) {
+	var match NodeMatch
+
+	if v := p.currentToken(); v.typ != tokenTypeKeyword || v.val != KeywordMatch {
+		return match, errors.New(`match node does not start with "match"`)
+	}
+
+	for !p.isEOF() {
+		token := p.currentToken()
+
+		if token.typ != tokenTypeKeyword {
+			return NodeMatch{}, fmt.Errorf("invalid match")
+		}
+
+		switch token.val {
+		case KeywordMatch:
+			p.pos++
+
+			expr, err := p.parseExpr()
+			if err != nil {
+				return NodeMatch{}, fmt.Errorf("parse expr: %w", err)
+			}
+
+			match.Selectors = append(match.Selectors, expr)
+		case KeywordWhen:
+			variant, err := p.parseVariant()
+			if err != nil {
+				return NodeMatch{}, fmt.Errorf("parse variant: %w", err)
+			}
+
+			match.Variants = append(match.Variants, variant)
+		}
+	}
+
+	return match, nil
+}
+
+func (p *parser) parseExpr() (NodeExpr, error) {
+	if p.currentToken().typ != tokenTypeSeparatorOpen {
+		return NodeExpr{}, errors.New(`expression does not start with "{"`)
+	}
+
+	var expr NodeExpr
+
+	for !p.isEOF() {
+		token := p.nextToken()
+
+		switch token.typ {
+		case tokenTypeVariable:
+			expr.Value = NodeVariable{Name: token.val}
+		case tokenTypeFunction:
+			function, err := p.parseFunction()
+			if err != nil {
+				return NodeExpr{}, fmt.Errorf("parse function: %w", err)
+			}
+
+			expr.Function = function
+		case tokenTypeSeparatorClose:
+			p.pos++
+
+			return expr, nil
+		case tokenTypeKeyword, tokenTypeSeparatorOpen,
+			tokenTypeLiteral, tokenTypeText,
+			tokenTypeEOF, tokenTypeError,
+			tokenTypeOpeningFunction, tokenTypeClosingFunction:
+		}
+	}
+
+	return NodeExpr{}, fmt.Errorf("invalid expression node")
+}
+
+func (p *parser) parseVariant() (NodeVariant, error) {
+	var variant NodeVariant
+
+	literal := p.nextToken()
+
+	if literal.typ != tokenTypeLiteral {
+		return NodeVariant{}, fmt.Errorf(`literal does not follow keyword "when"`)
+	}
+
+	variant.Keys = append(variant.Keys, literal.val)
+
+	p.pos++
+
+	text, err := p.parseText()
+	if err != nil {
+		return NodeVariant{}, fmt.Errorf("parse text: %w", err)
+	}
+
+	variant.Message = append(variant.Message, text...)
+
+	return variant, nil
+}
+
+func (p *parser) parseFunction() (NodeFunction, error) {
+	var function NodeFunction
+
+	if p.tokens[p.pos-1].typ != tokenTypeVariable {
+		return function, errors.New(`function does not follow variable`)
+	}
+
+	function.Name = p.currentToken().val
+
+	return function, nil
+}
+
+func (p *parser) parseVariable() (NodeVariable, error) {
+	p.pos++
+
+	var variable NodeVariable
+
+	if p.tokens[p.pos-1].typ != tokenTypeSeparatorOpen {
+		return variable, errors.New(`function does not follow placeholder open`)
+	}
+
+	variable.Name = p.currentToken().val
+
+	return variable, nil
+}
+
+func (p *parser) currentToken() Token {
+	return p.tokens[p.pos]
+}
+
+func (p *parser) nextToken() Token {
+	p.pos++
+
+	return p.tokens[p.pos]
+}
+
+func (p *parser) isEOF() bool { return p.tokens[p.pos].typ == tokenTypeEOF }
---
+++ pkg/messageformat/parse_test.go
@@ -0,0 +1,141 @@
+package messageformat
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Parse(t *testing.T) {
+	t.Parallel()
+
+	for _, test := range []struct {
+		name, input string
+		expectedErr error
+		expected    []interface{}
+	}{
+		{
+			name:     "empty",
+			input:    "",
+			expected: []interface{}(nil),
+		},
+		{
+			name:     "empty expr",
+			input:    "{}",
+			expected: []interface{}(nil),
+		},
+		{
+			name:     "expr with text",
+			input:    "{Hello, World!}",
+			expected: []interface{}{NodeText{Text: "Hello, World!"}},
+		},
+		{
+			name:  "match",
+			input: "match {$count} when * {Hello, world!}",
+			expected: []interface{}{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}}},
+					Variants: []NodeVariant{
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world!"}}},
+					},
+				},
+			},
+		},
+		{
+			name:  "match with function",
+			input: "match {$count :number} when * {Hello, world!}",
+			expected: []interface{}{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
+					Variants: []NodeVariant{
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world!"}}},
+					},
+				},
+			},
+		},
+		{
+			name:  "match with multiple variants",
+			input: "match {$count :number} when 1 {Hello, friend!} when * {Hello, friends!} ",
+			expected: []interface{}{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
+					Variants: []NodeVariant{
+						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Hello, friend!"}}},
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, friends!"}}},
+					},
+				},
+			},
+		},
+		{
+			name:  "match with plurals",
+			input: "match {$count :number} when 1 {Buy one apple!} when * {Buy {$count} apples!} ",
+			expected: []interface{}{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
+					Variants: []NodeVariant{
+						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Buy one apple!"}}},
+						{Keys: []string{"*"}, Message: []interface{}{
+							NodeText{Text: "Buy "},
+							NodeVariable{Name: "count"},
+							NodeText{Text: " apples!"},
+						}},
+					},
+				},
+			},
+		},
+
+		{
+			name: "match with two variables in variant",
+			input: "match {$count :number} " +
+				"when 0 {No apples!} " +
+				"when 1 {Buy {$count}{$counts} apple!} " +
+				"when * {Buy {$count} apples 2!} ",
+			expected: []interface{}{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
+					Variants: []NodeVariant{
+						{Keys: []string{"0"}, Message: []interface{}{NodeText{Text: "No apples!"}}},
+						{Keys: []string{"1"}, Message: []interface{}{
+							NodeText{Text: "Buy "},
+							NodeVariable{Name: "count"},
+							NodeVariable{Name: "counts"},
+							NodeText{Text: " apple!"},
+						}},
+						{Keys: []string{"*"}, Message: []interface{}{
+							NodeText{Text: "Buy "},
+							NodeVariable{Name: "count"},
+							NodeText{Text: " apples 2!"},
+						}},
+					},
+				},
+			},
+		},
+		{
+			name:        "invalid expr",
+			input:       "match $count :number} ",
+			expectedErr: fmt.Errorf("expression does not start with \"{\""),
+		},
+	} {
+		test := test
+
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+
+			l, err := Parse(test.input)
+
+			fmt.Println(test.expected)
+			fmt.Println(l)
+			fmt.Println(test.expectedErr)
+
+			if test.expectedErr != nil {
+				assert.Errorf(t, err, test.expectedErr.Error())
+				return
+			}
+
+			require.NoError(t, err)
+			assert.Equal(t, test.expected, l)
+		})
+	}
+}
