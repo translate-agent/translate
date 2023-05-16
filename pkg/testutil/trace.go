5f723adcac734ea1f5ea817cfde8d7de6203bbbd
Vladislavs <v.perkanuks@gmail.com>
2023-05-16 13:00:56 +0300

    fail manual span if test failed

--- pkg/testutil/trace.go
+++ pkg/testutil/trace.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 	"testing"
 
+	"go.opentelemetry.io/otel/codes"
 	"go.opentelemetry.io/otel/trace"
 )
 
@@ -22,27 +23,10 @@ func Trace(ctx context.Context, t *testing.T, tracer trace.Tracer) (context.Cont
 	spanEnd := func() {
 		defer span.End()
 
-		// // If the test failed, store the error in the global map.
-		// if err, ok := errMsgs.Load(t.Name()); ok {
-
-		// 	// TODO temporal solution to handle different error types.
-		// 	switch e := err.(type) {
-		// 	case traceErr:
-		// 		span.SetStatus(otelcodes.Error, e.description)
-		// 		span.RecordError(e.err)
-		// 	case error:
-		// 		span.SetStatus(otelcodes.Error, "")
-		// 		span.RecordError(err.(error))
-		// 	default:
-		// 		t.Fatalf("unknown error type: %T", err)
-		// 	}
-
-		// 	// If the test is a sub-test, then parent span should also be marked as failed.
-		// 	if len(testNameParts) > 1 {
-		// 		errMsgs.Store(testNameParts[0], errors.New("one or more sub-tests failed"))
-		// 	}
-
-		// }
+		if t.Failed() {
+			span.SetStatus(codes.Error, "")
+		}
 	}
+
 	return newCtx, spanEnd
 }
