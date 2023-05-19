a231c33ff03cfff9a95473c18e7d19d40bda24ee
Vladislavs <v.perkanuks@gmail.com>
2023-05-19 10:52:56 +0300

    Add comments to `Trace()`

--- pkg/testutil/trace.go
+++ pkg/testutil/trace.go
@@ -9,20 +9,23 @@ import (
 	"go.opentelemetry.io/otel/trace"
 )
 
+// Trace starts a new span using the provided Tracer.
+// The span name is derived from the provided testing.T object.
 func Trace(ctx context.Context, t *testing.T, tracer trace.Tracer) (context.Context, func()) {
+	// Split test name into sub-tests names.
 	testNameParts := strings.Split(t.Name(), "/")
 
-	// Get the sub-test name if it exists.
 	spanName := testNameParts[0]
-	if isSubTest := len(testNameParts) > 1; isSubTest {
+	// If the test name contains sub-tests, use the sub-test name as span name.
+	if len(testNameParts) > 1 {
 		spanName = testNameParts[len(testNameParts)-1]
 	}
 
 	newCtx, span := tracer.Start(ctx, spanName)
-
 	spanEnd := func() {
 		defer span.End()
 
+		// If the test failed, set the span status to Error
 		if t.Failed() {
 			span.SetStatus(codes.Error, "")
 		}
