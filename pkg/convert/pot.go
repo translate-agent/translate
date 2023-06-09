f090fb341815482fecb54950575a0ad1726ad731
DariaLeonova <darja.leonova.it@gmail.com>
2023-06-09 11:52:01 +0300

    added convert.go

--- pkg/convert/pot.go
+++ pkg/convert/pot.go
@@ -227,37 +227,6 @@ func getPoTagLines(str string) []string {
 	return lines
 }
 
-func convertToMessageFormatPlural(plurals []string) string {
-	var sb strings.Builder
-
-	sb.WriteString("match {$count :number}\n")
-
-	for i, plural := range plurals {
-		line := strings.ReplaceAll(strings.TrimSpace(plural), "%d", "{$count}")
-
-		var count string
-
-		switch i {
-		default:
-			count = fmt.Sprintf("%d", i+1)
-		case len(plurals) - 1:
-			count = "*"
-		}
-
-		sb.WriteString(fmt.Sprintf("when %s {%s}\n", count, line))
-	}
-
-	return sb.String()
-}
-
-func convertToMessageFormatSingular(message string) string {
-	if message != "" {
-		return "{" + message + "}"
-	}
-
-	return ""
-}
-
 func writeMessage(b *bytes.Buffer, index int, message model.Message) error {
 	if index > 0 {
 		if _, err := fmt.Fprint(b, "\n"); err != nil {
