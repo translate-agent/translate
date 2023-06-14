5879254e29acb6ff4ea20061e272af13edf58db4
Janis Klavins <janis.klavins@expect.digital>
2023-06-14 11:08:54 +0300

    tidy

--- cmd/translate/main.go
+++ cmd/translate/main.go
@@ -4,7 +4,6 @@ import (
 	"go.expect.digital/translate/cmd/translate/service"
 )
 
-// Execute adds all child commands to the root command and sets flags appropriately.
 func main() {
 	service.Serve()
 }
