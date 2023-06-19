5901f3d0e9dea9cef0163220f2d09b519e2878b9
DarjaLeonova <94634373+DarjaLeonova@users.noreply.github.com>
2023-06-19 13:05:58 +0300

    added MessageFormat support (#84)
    * added MessageFormat support

--- pkg/convert/convert.go
+++ pkg/convert/convert.go
@@ -1,5 +1,7 @@
 package convert
 
+import "strings"
+
 // convertToMessageFormatSingular wraps the input string with curly braces and returns the modified string.
 func convertToMessageFormatSingular(message string) string {
 	if message == "" {
@@ -7,4 +9,10 @@ func convertToMessageFormatSingular(message string) string {
 	}
 
 	return "{" + message + "}"
+}
+
+// removeEnclosingBrackets replaces '{' and '}', temporarily maintain only the singular form.
+func removeEnclosingBrackets(message string) string {
+	replacer := strings.NewReplacer("{", "", "}", "")
+	return replacer.Replace(message)
 }
