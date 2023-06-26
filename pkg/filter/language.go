503a44aa4970942f4f938407ac6cf9e7333780ec
Janis Klavins <janis.klavins@expect.digital>
2023-06-26 13:45:17 +0300

    tidy

--- pkg/filter/language.go
+++ pkg/filter/language.go
@@ -2,14 +2,14 @@ package filter
 
 import "golang.org/x/text/language"
 
-// LanguageTags removes duplicates returns slice of unique language tags.
+// LanguageTags removes duplicates and empty tags returns slice of unique language tags.
 func LanguageTags(lt []language.Tag) []language.Tag {
 	uniques := make([]language.Tag, 0, len(lt))
 
 	m := make(map[language.Tag]struct{}, len(lt))
 
 	for _, v := range lt {
-		if _, ok := m[v]; ok {
+		if _, ok := m[v]; ok || v == language.Und {
 			continue
 		}
 
