87e8ce6321dcf93b8b97192b6b23352082a8355f
Janis Klavins <janis.klavins@expect.digital>
2023-06-26 10:23:06 +0300

    tidy

--- pkg/testutil/compare.go
+++ pkg/testutil/compare.go
@@ -3,15 +3,13 @@ package testutil
 import (
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 	"go.expect.digital/translate/pkg/model"
 )
 
 func EqualMessages(t *testing.T, expected, actual *model.Messages) {
 	if expected == nil {
-		assert.Equal(t, expected, actual)
-		return
+		require.Equal(t, expected, actual)
 	}
 
 	require.Equal(t, expected.Language, actual.Language)
