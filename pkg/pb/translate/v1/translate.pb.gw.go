37d3ea0dad838ceb6486dc8008ff7af69b4bb342
Vladislavs <vladislavs.perkanuks@expect.digital>
2023-06-26 17:45:55 +0300

    Updated remote to plugin

--- pkg/pb/translate/v1/translate.pb.gw.go
+++ pkg/pb/translate/v1/translate.pb.gw.go
@@ -776,7 +776,7 @@ func RegisterTranslateServiceHandlerServer(ctx context.Context, mux *runtime.Ser
 // RegisterTranslateServiceHandlerFromEndpoint is same as RegisterTranslateServiceHandler but
 // automatically dials to "endpoint" and closes the connection when "ctx" gets done.
 func RegisterTranslateServiceHandlerFromEndpoint(ctx context.Context, mux *runtime.ServeMux, endpoint string, opts []grpc.DialOption) (err error) {
-	conn, err := grpc.Dial(endpoint, opts...)
+	conn, err := grpc.DialContext(ctx, endpoint, opts...)
 	if err != nil {
 		return err
 	}
