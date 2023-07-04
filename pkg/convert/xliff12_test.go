80021a007b97e7a8c71c1cd660a8cc3f47a54cb2
Vladislavs <vladislavs.perkanuks@expect.digital>
2023-07-04 13:24:36 +0300

    tidy

--- pkg/convert/xliff12_test.go
+++ pkg/convert/xliff12_test.go
@@ -139,11 +139,11 @@ func Test_TransformXLIFF12(t *testing.T) {
 		restoredMessages, err := FromXliff12(xliffData)
 		require.NoError(t, err)
 
-		testutil.EqualMessages(t, expected, &restoredMessages)
-
 		// TODO: for now restore the flag to the expected
 		// remove this as XLIFF is the format were we can implicitly determine if file is translated or not
 		restoredMessages.Original = expected.Original
+
+		testutil.EqualMessages(t, expected, &restoredMessages)
 
 		return true
 	}
