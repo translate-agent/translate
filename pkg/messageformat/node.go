933ec63dfdd4fcdbb8cef2dfe4c3623b493ea0c2
Vladislavs <vladislavs.perkanuks@expect.digital>
2023-10-31 15:27:33 +0200

    initial parser fix

--- pkg/messageformat/node.go
+++ pkg/messageformat/node.go
@@ -1,5 +1,11 @@
 package messageformat
 
+type AST []interface{}
+
+func (n *AST) Append(node ...interface{}) {
+	*n = append(*n, node...)
+}
+
 type NodeMatch struct {
 	Selectors []NodeExpr
 	Variants  []NodeVariant
@@ -28,5 +34,12 @@ type NodeVariant struct {
 }
 
 type NodeFunction struct {
-	Name string
+	Name    string
+	Options []NodeOption
+}
+
+// https://github.com/unicode-org/message-format-wg/blob/main/spec/syntax.md#options
+type NodeOption struct {
+	Value interface{}
+	Name  string
 }
--- pkg/messageformat/parse_test.go
+++ pkg/messageformat/parse_test.go
@@ -1,136 +1,225 @@
 package messageformat
 
 import (
-	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
+//nolint:lll
 func Test_Parse(t *testing.T) {
 	t.Parallel()
 
 	for _, test := range []struct {
-		name, input string
-		expectedErr error
-		expected    []interface{}
+		name     string
+		input    string
+		expected AST
 	}{
+		// singular tests
 		{
 			name:     "empty",
 			input:    "",
-			expected: []interface{}(nil),
+			expected: AST(nil),
 		},
 		{
 			name:     "empty expr",
 			input:    "{}",
-			expected: []interface{}(nil),
+			expected: AST(nil),
 		},
 		{
-			name:     "expr with text",
-			input:    "{Hello, World!}",
-			expected: []interface{}{NodeText{Text: "Hello, World!"}},
+			name:     "text",
+			input:    "{Hello, World}",
+			expected: AST{NodeText{Text: "Hello, World"}},
 		},
 		{
-			name:  "match",
-			input: "match {$count} when * {Hello, world!}",
-			expected: []interface{}{
+			name:     "text with escaped curly braces",
+			input:    "{Hello, \\{World\\}}",
+			expected: AST{NodeText{Text: "Hello, \\{World\\}"}},
+		},
+		{
+			name:     "text with variable",
+			input:    "{Hello {$var} World}",
+			expected: AST{NodeText{Text: "Hello "}, NodeVariable{Name: "var"}, NodeText{Text: " World"}},
+		},
+		{
+			name:     "text with function",
+			input:    "{Hello {:func} World}",
+			expected: AST{NodeText{Text: "Hello "}, NodeFunction{Name: "func"}, NodeText{Text: " World"}},
+		},
+		{
+			name:  "extracted placeholder pythonVar",
+			input: "{{:Placeholder name=object format=pythonVar type=string} does not exist in this database.}",
+			expected: AST{
+				NodeExpr{
+					Value: nil,
+					Function: NodeFunction{
+						Name: "Placeholder",
+						Options: []NodeOption{
+							{Name: "name", Value: "object"},
+							{Name: "format", Value: "pythonVar"},
+							{Name: "type", Value: "string"},
+						},
+					},
+				},
+				NodeText{Text: " does not exist in this database."},
+			},
+		},
+		{
+			name:  "extracted placeholders printf style",
+			input: "{{:Placeholder format=printf type=string} does not exist in {:Placeholder format=printf type=int}. database.}}",
+			expected: AST{
+				NodeExpr{
+					Value: nil,
+					Function: NodeFunction{
+						Name: "Placeholder",
+						Options: []NodeOption{
+							{Name: "format", Value: "printf"},
+							{Name: "type", Value: "string"},
+						},
+					},
+				},
+				NodeText{Text: " does not exist in "},
+				NodeExpr{
+					Value: nil,
+					Function: NodeFunction{
+						Name: "Placeholder",
+						Options: []NodeOption{
+							{Name: "format", Value: "printf"},
+							{Name: "type", Value: "int"},
+						},
+					},
+				},
+				NodeText{Text: ". database."},
+			},
+		},
+		// plural tests
+		{
+			name:  "single match",
+			input: "match {$count} when * {Hello, world\\!}",
+			expected: AST{
 				NodeMatch{
 					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}}},
 					Variants: []NodeVariant{
-						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world!"}}},
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world\\!"}}},
 					},
 				},
 			},
 		},
 		{
-			name:  "match with function",
-			input: "match {$count :number} when * {Hello, world!}",
-			expected: []interface{}{
+			name:  "single match with function",
+			input: "match {$count :number} when * {Hello, world\\!}",
+			expected: AST{
 				NodeMatch{
 					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
 					Variants: []NodeVariant{
-						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world!"}}},
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, world\\!"}}},
 					},
 				},
 			},
 		},
 		{
 			name:  "match with multiple variants",
-			input: "match {$count :number} when 1 {Hello, friend!} when * {Hello, friends!} ",
-			expected: []interface{}{
+			input: "match {$count :number} when 1 {Hello, friend\\!} when * {Hello, friends\\!} ",
+			expected: AST{
 				NodeMatch{
 					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
 					Variants: []NodeVariant{
-						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Hello, friend!"}}},
-						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, friends!"}}},
+						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Hello, friend\\!"}}},
+						{Keys: []string{"*"}, Message: []interface{}{NodeText{Text: "Hello, friends\\!"}}},
 					},
 				},
 			},
 		},
 		{
 			name:  "match with plurals",
-			input: "match {$count :number} when 1 {Buy one \\\\ apple!} when * {Buy {$count} apples!} ",
-			expected: []interface{}{
+			input: "match {$count :number} when 1 {Buy one \\\\ apple\\!} when * {Buy {$count} apples\\!} ",
+			expected: AST{
 				NodeMatch{
 					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
 					Variants: []NodeVariant{
-						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Buy one \\\\ apple!"}}},
+						{Keys: []string{"1"}, Message: []interface{}{NodeText{Text: "Buy one \\\\ apple\\!"}}},
 						{Keys: []string{"*"}, Message: []interface{}{
 							NodeText{Text: "Buy "},
 							NodeVariable{Name: "count"},
-							NodeText{Text: " apples!"},
+							NodeText{Text: " apples\\!"},
 						}},
 					},
 				},
 			},
 		},
-
 		{
 			name: "match with two variables in variant",
 			input: "match {$count :number} " +
-				"when 0 {No apples!} " +
-				"when 1 {Buy {$count}{$counts} apple!} " +
-				"when * {Buy {$count} apples 2!} ",
-			expected: []interface{}{
+				"when 0 {No apples\\!} " +
+				"when 1 {Buy {$count}{$counts} apple\\!} " +
+				"when * {Buy {$count} apples 2\\!} ",
+			expected: AST{
 				NodeMatch{
 					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
 					Variants: []NodeVariant{
-						{Keys: []string{"0"}, Message: []interface{}{NodeText{Text: "No apples!"}}},
+						{Keys: []string{"0"}, Message: []interface{}{NodeText{Text: "No apples\\!"}}},
 						{Keys: []string{"1"}, Message: []interface{}{
 							NodeText{Text: "Buy "},
 							NodeVariable{Name: "count"},
 							NodeVariable{Name: "counts"},
-							NodeText{Text: " apple!"},
+							NodeText{Text: " apple\\!"},
 						}},
 						{Keys: []string{"*"}, Message: []interface{}{
 							NodeText{Text: "Buy "},
 							NodeVariable{Name: "count"},
-							NodeText{Text: " apples 2!"},
+							NodeText{Text: " apples 2\\!"},
 						}},
 					},
 				},
 			},
 		},
 		{
-			name:        "invalid expr",
-			input:       "match $count :number} ",
-			expectedErr: fmt.Errorf("expression does not start with \"{\""),
-		},
-		{
-			name:     "input with curly braces in it",
-			input:    `{Chart [\{\}] was added to dashboard [\{\}]}`,
-			expected: []interface{}{NodeText{Text: "Chart [\\{\\}] was added to dashboard [\\{\\}]"}},
-		},
-		{
-			name:     "input with plus sign in it ",
-			input:    `{+ vēl %s}`,
-			expected: []interface{}{NodeText{Text: "+ vēl %s"}},
-		},
-		{
-			name:     "input with minus sign in it ",
-			input:    `{- vēl %s}`,
-			expected: []interface{}{NodeText{Text: "- vēl %s"}},
+			name: "match plural with extracted bracketVar placeholder",
+			input: "match {$count :number} " +
+				"when 1 {Were having trouble loading this visualization. Queries are set to timeout after {:Placeholder name=sec format=bracketVar} second.}" +
+				"when * {Were having trouble loading this visualization. Queries are set to timeout after {:Placeholder name=sec format=bracketVar} seconds.}",
+			expected: AST{
+				NodeMatch{
+					Selectors: []NodeExpr{{Value: NodeVariable{Name: "count"}, Function: NodeFunction{Name: "number"}}},
+					Variants: []NodeVariant{
+						{
+							Keys: []string{"1"},
+							Message: []interface{}{
+								NodeText{Text: "Were having trouble loading this visualization. Queries are set to timeout after "},
+								NodeExpr{
+									Value: nil,
+									Function: NodeFunction{
+										Name: "Placeholder",
+										Options: []NodeOption{
+											{Name: "name", Value: "sec"},
+											{Name: "format", Value: "bracketVar"},
+										},
+									},
+								},
+								NodeText{Text: " second."},
+							},
+						},
+						{
+							Keys: []string{"*"},
+							Message: []interface{}{
+								NodeText{Text: "Were having trouble loading this visualization. Queries are set to timeout after "},
+								NodeExpr{
+									Value: nil,
+									Function: NodeFunction{
+										Name: "Placeholder",
+										Options: []NodeOption{
+											{Name: "name", Value: "sec"},
+											{Name: "format", Value: "bracketVar"},
+										},
+									},
+								},
+								NodeText{Text: " seconds."},
+							},
+						},
+					},
+				},
+			},
 		},
 	} {
 		test := test
@@ -139,13 +228,8 @@ func Test_Parse(t *testing.T) {
 			t.Parallel()
 
 			l, err := Parse(test.input)
-
-			if test.expectedErr != nil {
-				require.Errorf(t, err, test.expectedErr.Error())
-				return
-			}
-
 			require.NoError(t, err)
+
 			assert.Equal(t, test.expected, l)
 		})
 	}
