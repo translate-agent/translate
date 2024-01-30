74c98d1c54161534b602470f248dde8cec8c6b5c
DariaLeonova <darja.leonova.it@gmail.com>
2024-01-30 09:56:11 +0200

    tidy

--- pkg/fuzzy/google_translate.go
+++ pkg/fuzzy/google_translate.go
@@ -271,7 +271,7 @@ func patternToString(pattern []ast.Pattern) string {
 		switch v := pattern[i].(type) {
 		case ast.TextPattern:
 			text += string(v)
-		case ast.LiteralExpression, ast.AnnotationExpression, ast.VariableExpression, ast.Markup: // or default
+		case ast.LiteralExpression, ast.AnnotationExpression, ast.VariableExpression, ast.Markup:
 			text += fmt.Sprintf("{$%d}", i)
 		}
 	}
